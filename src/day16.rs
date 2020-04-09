52991484946bf1245edcabc5dce36aa7a8493beb
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-04-09 19:09:30 +0200

    Day17

--- src/day16.rs
+++ src/day16.rs
@@ -4,16 +4,17 @@ use itertools::Itertools;
 
 pub const PARTS: [fn(); 2] = [part1, part2];
 
+const INPUT: &[u8] = b"01111010110010011";
+
 fn dragon<'a, T: DoubleEndedIterator<Item = bool> + Clone + 'a>(
     it: T,
-) -> impl DoubleEndedIterator<Item = bool> + Clone {
+) -> impl Iterator<Item = bool> {
     it.clone()
         .chain(iter::once(false))
         .chain(it.map(|x| !x).rev())
 }
 
 fn part1() {
-    const INPUT: &[u8] = b"272";
     const DISK: usize = 272;
     //
     let mut a = INPUT.iter().map(|&x| x != b'0').collect::<Vec<_>>();
@@ -48,7 +49,6 @@ fn part1() {
 }
 
 fn part2() {
-    const INPUT: &[u8] = b"01111010110010011";
     const DISK: usize = 35651584;
     //
     let mut a = INPUT.iter().map(|&x| x != b'0').collect::<Vec<_>>();
